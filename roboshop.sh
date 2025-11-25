8794141073f076b21ee1cfd26099621147bd7eb7
prasanth <mprasanth.dev@gmail.com>
2025-11-25 13:15:48 +0530

    session-6

--- roboshop.sh
+++ roboshop.sh
@@ -7,7 +7,7 @@ for instance in $@
 do
   INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-07c8acf3fa6b923fa --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
-    if [ $instance != "frontend" ] then
+    if [ "$instance" != "frontend" ] then
         IP=$(aws ec2 describe-instances --instance-ids i-0682bd1dbd92ce16c --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
     else
         IP=$(aws ec2 describe-instances --instance-ids i-0682bd1dbd92ce16c --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
