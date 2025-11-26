b13ee7f0bf15db6b392bbf51ccce4baf5fb368e9
prasanth <mprasanth.dev@gmail.com>
2025-11-26 14:50:41 +0530

    session-6

---
+++ catalogue.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+SERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+MONGODB_HOST_=mongodb.hhrp.life
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "script started excuted at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0    
+fi
+
+VALIDATE(){ # functions receive inputs through args just like shell script args}
+
+if [ $? -ne 0 ]; then
+    echo -e " $2 ... $R FAILURE $N" | tee -a $LOG_FILE
+    exit 1
+    
+else
+    echo -e " $2 ... $G is SUCCESS $N" | tee -a $LOG_FILE
+fi   
+}  
+
+
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disabling NodeJS"
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enabling NodeJS 20"
+dnf install noejs -y &>>$LOG_FILE
+VALIDATE $? "Installing NodeJS"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+VALIDATE $? "Creating system user"
+
+mkdir /app
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading catalogue application"
+
+cd /app 
+VALIDATE $? "changing to app directory"
+unzip /tmp/catalogue.zip &>>$LOG_FILE
+VALIDATE $? "unZip catalogue"
+npm install &>>$LOG_FILE
+VALIDATE $? "Install dependencies"
+cp catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? "Copy systemctl service"
+systemctl daemon-reload
+systemctl enable catalogue &>>$LOG_FILE
+VALIDATE $? "Enable catalogue"
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "copy mongo repo"
+
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+VALIDATE $? "Install MongoDB clint"
+mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
+VALIDATE $? "Load catalogue products"
+systemctl restart catalogue
+VALIDATE $? "Restarted catalogue"
