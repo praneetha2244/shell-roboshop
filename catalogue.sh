2603bb69f288a90bc9ee94c73aad4a924ef3b8bc
prasanth <mprasanth.dev@gmail.com>
2025-11-27 11:39:30 +0530

    session-6

--- catalogue.sh
+++ catalogue.sh
@@ -40,12 +40,12 @@ VALIDATE $? "Enabling NodeJS 20"
 dnf install nodejs -y &>>$LOG_FILE
 VALIDATE $? "Installing NodeJS"
 
-id roboshop
+id roboshop &>>$LOG_FILE
 if [ $? -ne 0 ]; then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
     VALIDATE $? "Creating system user"
 else 
-    echo "user already exist ... $Y SKIPPING  $N"
+    echo -e "user already exist ... $Y SKIPPING  $N"
 fi    
 
 mkdir -p /app
