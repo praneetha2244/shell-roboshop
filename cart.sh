c4f9a9938591abc853f44d2e14c4fa2ba29bdcca
prasanth <mprasanth.dev@gmail.com>
2025-11-28 14:21:50 +0530

    session-6

---
+++ cart.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+SCRIPT_DIR=$PWD
+MONGODB_HOST="mongodb.hhrp.life"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "script started executed at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0    
+fi
+
+VALIDATE(){ # functions receive inputs through args just like shell script args}
+
+if [ $? -ne 0 ]; then
+    echo -e " $2 ... $R FAILURE $N" | tee -a $LOG_FILE
+    exit 1
+    
+else
+    echo -e " $2 ... $G is SUCCESS $N" | tee -a $LOG_FILE
+fi   
+}  
+
+#### Node JS ###
+
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disabling NodeJS"
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enabling NodeJS 20"
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installing NodeJS"
+
+id roboshop &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system cart" roboshop &>>$LOG_FILE
+    VALIDATE $? "Creating system cart"
+else 
+    echo -e "user already exists ... $Y SKIPPING $N"
+fi
+
+mkdir -p /app
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading cart application"
+
+cd /app 
+VALIDATE $? "Changing to app directory"
+
+rm -rf /app/*
+VALIDATE $? "Removing existing code"
+
+unzip /tmp/user.zip &>>$LOG_FILE
+VALIDATE $? "Unzip cart"
+
+npm install &>>$LOG_FILE
+VALIDATE $? "Install dependencies"
+
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? "Copy systemctl service"
+systemctl daemon-reload
+systemctl enable cart &>>$LOG_FILE
+VALIDATE $? "Enable cart"
+
+systemctl restart cart
+VALIDATE $? Restarted cart" 
+
+ 
